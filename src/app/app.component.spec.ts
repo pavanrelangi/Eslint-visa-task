ec3ef41b887a4ac3dd6a85bcdba884a0fde4ce63
Pavan Babul Gowd Relangi <11202132@in.techwave.net>
2021-09-23 11:17:36 +0530

    eslint

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'eslint'`, () => {
+  it('should have as title \'eslint\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('eslint');
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'eslint';
+
+export class AppComponent implements OnInit {
+  title='eslint';
+  data=`haiii`
+  doublesemicol="check";;
+
+   colors = [ "red",, "blue" ];
+   //   ngoninit   /////
+  ngOnInit(){
+    console.log('hello ');
+    var data=2//data is 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
 }
